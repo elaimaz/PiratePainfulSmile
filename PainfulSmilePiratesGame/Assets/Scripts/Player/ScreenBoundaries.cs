d14346930fb5a214d0ec4189b50120b3990cc1c0
Wesley <fariawesley4@gmail.com>
2020-09-14 01:09:55 -0300

    Fix #21

---
+++ PainfulSmilePiratesGame/Assets/Scripts/Player/ScreenBoundaries.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScreenBoundaries : MonoBehaviour
+{
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1, screenBounds.x);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1, screenBounds.y);
+        transform.position = viewPos;
+    }
+}
