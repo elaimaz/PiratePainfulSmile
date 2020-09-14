0734837005362452d756d3e61c76b0ca7dce29ae
Wesley <fariawesley4@gmail.com>
2020-09-14 16:16:09 -0300

    Fix #25
    explosion appears when cannon ball touch enemy ship

---
+++ PainfulSmilePiratesGame/Assets/Scripts/Objects/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] explosionSprites;
+    private int currentIndex = 0;
+    private float timer;
+    [SerializeField]
+    private float animationRate = 0.1f;
+    private bool runAnimation = true;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer >= animationRate && runAnimation != false)
+        {
+            timer -= animationRate;
+            GetComponentInChildren<SpriteRenderer>().sprite = explosionSprites[currentIndex];
+            currentIndex = (currentIndex + 1);
+            if (currentIndex >= explosionSprites.Length)
+                runAnimation = false;
+        }
+        else if (timer >= animationRate && runAnimation == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
